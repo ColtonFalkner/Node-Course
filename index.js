fb70f774295f56129a595659b9e58d0140c7117f
Colton <coltonfalkner@gmail.com>
2021-10-09 12:43:30 -0400

    first async function

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const fs = require('fs')
 // const hello = 'Hello World!'
 // console.log(hello)
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
-console.log(textIn)
+//Blocking Synchrnous Way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textIn)
 
-const textOut = `This is what we know about the avocado: ${textIn}. \n Created on ${Date.now()}`
-fs.writeFileSync('./txt/output.txt', textOut)
-console.log('File has been written')
+// const textOut = `This is what we know about the avocado: ${textIn}. \n Created on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log('File has been written')
+
+//Non-Blocking Async
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+  console.log(data)
+})
+console.log('Will read file...')
